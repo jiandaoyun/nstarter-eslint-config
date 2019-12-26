581cf8c766f91a037813e85f4d37423e5dddd625
kyle <kyle@fanruan.com>
2019-12-26 14:56:03 +0800

    限制 require-jsdoc

---
+++ test/node/require-jsdoc/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    rules: {
+        /**
+         * 限制一行中的语句数量
+         */
+        'require-jsdoc': [
+            'error',
+            {
+                require: {
+                    FunctionDeclaration: true,
+                    MethodDefinition: false,
+                    ClassDeclaration: false,
+                    ArrowFunctionExpression: false,
+                    FunctionExpression: false
+                }
+            }
+        ]
+    }
+};
